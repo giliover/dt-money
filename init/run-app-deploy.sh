cb975b6266771140d687a624cf37434be1c9dea1
giliover <giliover@hotmail.com>
2021-06-07 17:37:28 -0300

    Fix: add scriptdir variable

--- init/run-app-deploy.sh
+++ init/run-app-deploy.sh
@@ -2,13 +2,16 @@
 if [ $1 == "--init-dev" ]; then
     echo "Starting Development Environment Moving Containers Down..."
     echo "Deconstructing containers, if any..."
-    docker-compose -f $(pwd)/docker-compose.yml down
+    scriptdir=`dirname "$BASH_SOURCE"`
+    docker-compose -f $scriptdir/docker-compose.yml down
+    scriptdir=`dirname "$BASH_SOURCE"`
     echo "Building development containers..."
-    docker-compose -f $(pwd)/docker-compose.yml up -d --build
+    docker-compose -f $scriptdir/docker-compose.yml up -d --build
 fi
 if [ $1 == "--dev" ]; then
     echo "Deploying in development environment"
     
     echo "Building development containers "
-    docker-compose -f $(pwd)/docker-compose.yml up -d --build
+    scriptdir=`dirname "$BASH_SOURCE"`
+    docker-compose -f $scriptdir/docker-compose.yml up -d --build
 fi
