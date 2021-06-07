56a41ba28b124dc115baca9a070d29a67939c913
giliover <giliover@hotmail.com>
2021-06-07 17:35:22 -0300

    Chore: add Dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "components/Header";
+import Dashboard from "components/Dashboard";
 import GlobalStyle from "styles/global";
 
 function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
