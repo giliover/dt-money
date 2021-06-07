3bf663d8de5495cae8b6ba6f68876449c20df093
giliover <giliover@hotmail.com>
2021-06-07 11:53:52 -0300

    Chore: add shell script for application deployment with docker

---
+++ init/run-app-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ $1 == "--init-dev" ]; then
+    echo "Starting Development Environment Moving Containers Down..."
+    echo "Deconstructing containers, if any..."
+    docker-compose -f docker-compose.yml down
+    echo "Building development containers..."
+    docker-compose -f docker-compose.yml up -d --build
+fi
+if [ $1 == "--dev" ]; then
+    echo "Deploying in development environment"
+    
+    echo "Building development containers "
+    docker-compose -f docker-compose.yml up -d --build
+fi
