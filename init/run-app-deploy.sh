70e3f329dcca87dcd59a9f49b6752725be11bfd7
giliover <giliover@hotmail.com>
2021-06-07 12:26:19 -0300

    Fix: file location

--- init/run-app-deploy.sh
+++ init/run-app-deploy.sh
@@ -2,13 +2,13 @@
 if [ $1 == "--init-dev" ]; then
     echo "Starting Development Environment Moving Containers Down..."
     echo "Deconstructing containers, if any..."
-    docker-compose -f docker-compose.yml down
+    docker-compose -f $(pwd)/docker-compose.yml down
     echo "Building development containers..."
-    docker-compose -f docker-compose.yml up -d --build
+    docker-compose -f $(pwd)/docker-compose.yml up -d --build
 fi
 if [ $1 == "--dev" ]; then
     echo "Deploying in development environment"
     
     echo "Building development containers "
-    docker-compose -f docker-compose.yml up -d --build
+    docker-compose -f $(pwd)/docker-compose.yml up -d --build
 fi
