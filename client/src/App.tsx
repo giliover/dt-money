a8224014c6cfd2e53c379339aafdccad0acf8f37
giliover <giliover@hotmail.com>
2021-06-13 22:08:48 -0300

    Chore: add NewTransactionModal component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "components/Header";
 import Dashboard from "components/Dashboard";
+import NewTransactionModal from "components/NewTransactionModal";
 import GlobalStyle from "styles/global";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Dashboard />
       <GlobalStyle />
+      <NewTransactionModal />
     </>
   );
 }
