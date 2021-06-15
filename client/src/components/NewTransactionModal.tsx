92a070dc791302b4920aa57e3791edd36287775c
giliover <giliover@hotmail.com>
2021-06-14 23:00:03 -0300

    Chore: add type button functionality

--- client/src/components/NewTransactionModal.tsx
+++ client/src/components/NewTransactionModal.tsx
@@ -2,9 +2,15 @@ import React, { useRef, useState, useCallback } from "react";
 import { uuid } from "uuidv4";
 import useModals from "hooks/useModals";
 import useTransactions from "hooks/useTransactions";
+import incomeImg from "assets/income.svg";
+import outcomeImg from "assets/outcome.svg";
 import Modal from "react-modal";
 
-import { Container } from "styles/newTransactionModal";
+import {
+	Container,
+	TransactionTypeContainer,
+	RadioBox,
+} from "styles/newTransactionModal";
 
 import closeImg from "assets/close.svg";
 
@@ -15,6 +21,7 @@ function NewTransactionsModal() {
 	const [category, setCategory] = useState("");
 	const [title, setTitle] = useState("");
 	const [value, setValue] = useState(0);
+	const [type, setType] = useState("deposit");
 	const { addTransaction } = useTransactions();
 	const {
 		isNewTransactionModalOpen: isOpen,
@@ -30,7 +37,7 @@ function NewTransactionsModal() {
 					category,
 					value,
 					title,
-					type: "withdraw",
+					type,
 					createdAt: new Date(),
 				};
 				await addTransaction(data);
@@ -56,23 +63,47 @@ function NewTransactionsModal() {
 		>
 			<Container ref={formRef} onSubmit={handleSubmit}>
 				<h2>New Transaction</h2>
+
 				<button className="modal-close" onClick={onRequestClose}>
 					<img src={closeImg} alt="close" />
 				</button>
+
 				<input
-					onChange={(event) => setCategory(event.target.value)}
+					onChange={(event) => setTitle(event.target.value)}
 					type="text"
-					placeholder="category"
+					placeholder="title"
 				/>
+
 				<input
 					onChange={(event) => setValue(Number(event.target.value))}
 					type="number"
 					placeholder="value"
 				/>
+
+				<TransactionTypeContainer>
+					<RadioBox
+						type="button"
+						isActive={type === "deposit"}
+						onClick={() => setType("deposit")}
+					>
+						<img src={incomeImg} alt="input" />
+						<span>Input</span>
+					</RadioBox>
+
+					<RadioBox
+						type="button"
+						isActive={type === "withdraw"}
+						onClick={() => setType("withdraw")}
+					>
+						<img src={outcomeImg} alt="output" />
+						<span>Output</span>
+					</RadioBox>
+				</TransactionTypeContainer>
+
 				<input
-					onChange={(event) => setTitle(event.target.value)}
+					onChange={(event) => setCategory(event.target.value)}
 					type="text"
-					placeholder="title"
+					placeholder="category"
 				/>
 				<button type="submit">Confirm</button>
 			</Container>
--- client/src/styles/newTransactionModal.ts
+++ client/src/styles/newTransactionModal.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken } from "polished";
 
 export const Container = styled.form`
 	h2 {
@@ -40,5 +41,44 @@ export const Container = styled.form`
 		&:hover {
 			filter: brightness(0.9);
 		}
+	}
+`;
+
+export const TransactionTypeContainer = styled.div`
+	margin: 1rem 0;
+	display: grid;
+	grid-template-columns: 1fr 1fr;
+	gap: 0.5rem;
+`;
+
+interface RadioBoxProperties {
+	isActive: boolean;
+}
+
+export const RadioBox = styled.button<RadioBoxProperties>`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 4rem;
+	background: ${(props) => (props.isActive ? "#eee" : "transparent")};
+	border-radius: 0.25rem;
+	border: 1px solid var(--border-input);
+
+	transition: 0.2s;
+
+	&:hover {
+		border-color: ${darken(0.1, "#d7d7d7")};
+	}
+
+	img {
+		width: 20px;
+		height: 20px;
+	}
+
+	span {
+		display: inline-block;
+		margin-left: 1rem;
+		font-size: 1rem;
+		color: var(--input-text);
 	}
 `;
